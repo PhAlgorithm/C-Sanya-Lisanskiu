5337ddc47a0008b85dd6d0bc80b8a35acf5ce7f1
A Lis <lisanskiu_86@mail.ru>
2017-05-17 21:22:09 +0300

    555

---
+++ Lesson 20/Lesson20-1/CsvSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionLinq
+{
+    public static class CsvSerializer
+    {
+        public static string Serialize(Book obj) {
+            Type bookType = typeof(Book);
+            var propertiesToSerialize = bookType.GetProperties().Where(p => p.GetCustomAttributes(typeof(NoCsvAttribute), true).Length == 0);
+
+            StringBuilder newString = new StringBuilder();
+            foreach (PropertyInfo prop in propertiesToSerialize) {
+                newString.Append(prop.GetValue(obj)).Append(",");
+            }
+
+            return newString.Remove(newString.Length - 1, 1).ToString();
+        }
+
+        public static Book Deserialize(string str)
+        {
+            var array = str.Split(',');
+            Type bookType = typeof(Book);
+            
+            return new Book
+            {
+                Author = array[0],
+                Year = int.Parse(array[1]),
+                Text = array[2]
+            };
+        }
+    }
+}
---
+++ Lesson 20/Lesson20-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Collections;
+using System.Diagnostics;
+
+namespace ReflectionLinq
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Book first = new Book() { Author = "user1", Year = 1553, Text = "asdfgsdgdfg" };
+
+            string serializedBook = CsvSerializer.Serialize(first);
+
+            Book deserializedBook = CsvSerializer.Deserialize(serializedBook);
+
+            Type bookType = typeof(Book);
+
+            foreach (var prop in bookType.GetProperties()) {
+                Console.WriteLine(prop.Name);
+                if (prop.GetCustomAttribute<NoCsvAttribute>() != null) {
+                    Console.WriteLine("NoCsvAttribute");
+                }
+            }
+
+        }
+    }
+}
