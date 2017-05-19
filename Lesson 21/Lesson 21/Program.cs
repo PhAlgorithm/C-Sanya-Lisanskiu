25cb7ae2ed42461cbc7280458bbacbe1cc9cd01c
A Lis <lisanskiu_86@mail.ru>
2017-05-19 19:59:09 +0300

    Test

---
+++ Lesson 21/Lesson 21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lesson_21
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 51; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    Console.WriteLine(i+j);
+                }
+            }
+
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            Trace.Write("I am from Trase");
+
+            Debug.Write("ghgh ggh ");
+
+#if DEBUG
+            Console.WriteLine("it is debug");
+#endif
+        }
+
+
+
+    }
+}
---
+++ Lesson 21/ReflactionTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lesson_21;
+
+namespace ReflactionTest
+{
+    [TestClass]
+    public class MyClassTest
+    {
+        [TestMethod]
+        public void TestMethod1_assdad_sfsfs()
+        {
+            //Arreng
+            MyClass calc = new MyClass();
+
+            //Act
+
+            int result = calc.Dev(10, 5);
+            //Assert
+
+            Assert.AreEqual(2, result);
+
+        }
+    }
+}
