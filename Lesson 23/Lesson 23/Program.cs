4023831a3f81a7285b5c9da8272424e84d30219c
A Lis <lisanskiu_86@mail.ru>
2017-05-24 21:19:02 +0300

    rrr

---
+++ Lesson 23/Lesson 23/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader ReadFile = File.OpenText(@"text.txt");
+
+            string Input = null;
+                       
+            while ((Input = ReadFile.ReadLine()) != null)
+            {
+                var dic = Input.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+                Console.Write(String.Join(" ", dic));
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
