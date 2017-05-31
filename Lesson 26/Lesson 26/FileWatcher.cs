e098294d6021a163df5051903f7ffcc4c30b8936
A Lis <lisanskiu_86@mail.ru>
2017-05-31 19:49:57 +0300

    Modul 4

---
+++ Lesson 26/Lesson 26/FileWatcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lesson_26
+{
+    public class FileWatcher
+    {
+        private DateTime TimeChek { get; set; }
+
+        private string NameFile { get; set; }
+
+        public FileWatcher(string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            TimeChek = File.GetLastWriteTime(path);
+            NameFile = path;
+        }
+
+        public event ActionCheck ModFile;
+
+        public delegate void ActionCheck();
+
+        public void CheckFile()
+        {
+            DateTime TimeForCheck = File.GetLastWriteTime(NameFile);
+
+            if (TimeChek != TimeForCheck)
+            {
+                Console.WriteLine("The file was modified");
+            }
+        }
+    }
+}
---
+++ Lesson 26/Lesson 26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileWatcher fw = new FileWatcher("test.txt");
+
+            fw.CheckFile();
+
+            fw.ModFile += fw.CheckFile;
+
+            Console.ReadKey();
+        }
+    }
+}
